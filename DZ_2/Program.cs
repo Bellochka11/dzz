0521664c56becd0713e2ef43d15c7ec35199c628
Elizaveta <Elizavetabelyankina@mail.ru>
2023-01-31 16:22:40 +0400

    Задача 27

---
+++ DZ_2/Program.cs
@@ -0,0 +1,32 @@
+﻿//   Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int len(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+int summary(int a)
+{
+    int result = 0;
+    int dlina = len(a);
+    for (int i = 0; i < dlina; i++)
+    {
+        result += a % 10;
+        a /= 10;
+    }
+    return result;
+}
+
+Console.Write("Введите  А: ");
+int a = int.Parse(Console.ReadLine()!);
+int sum = summary(a);
+Console.WriteLine($"{sum}");
