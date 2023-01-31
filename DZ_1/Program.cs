6d02537262e4d0daffb6fa8672693d65c0a0dbe6
Elizaveta <Elizavetabelyankina@mail.ru>
2023-01-31 16:22:19 +0400

    Задача 25

---
+++ DZ_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// // Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int Degree(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+Console.Write("Введите  А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите B: ");
+int b = int.Parse(Console.ReadLine()!);
+int num_degree = Degree(a,b);
+Console.WriteLine($"{num_degree}");
